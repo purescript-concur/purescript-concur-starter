c4267d4d7a013a4980fdeae71711629ad74a906f
Anupam Jain <ajnsit@gmail.com>
2019-03-12 22:54:34 +0530

    Do more in the hello world example

--- src/Main.purs
+++ src/Main.purs
@@ -5,11 +5,14 @@ import Prelude
 import Concur.Core (Widget)
 import Concur.React (HTML)
 import Concur.React.DOM as D
+import Concur.React.Props as P
 import Concur.React.Run (runWidgetInDom)
 import Effect (Effect)
 
-rootWidget :: forall a. Widget HTML a
-rootWidget = D.text "Hello World"
+hello :: forall a. Widget HTML a
+hello = do
+  void $ D.button [P.onClick] [D.text "Say Hello"]
+  D.text "Hello Sailor!"
 
 main :: Effect Unit
-main = runWidgetInDom "root" rootWidget
+main = runWidgetInDom "root" hello
